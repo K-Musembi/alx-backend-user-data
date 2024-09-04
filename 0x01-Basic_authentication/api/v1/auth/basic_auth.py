f68eb9cb0c404fe85c1a989466281d4a7bb268f3
K-Musembi <kevinmusembi.m@gmail.com>
2024-09-04 12:50:10 +0300

    complete basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,9 @@
 """basic auth module"""
 
 from api.v1.auth.auth import Auth
+import base64
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -27,3 +30,71 @@ class BasicAuth(Auth):
             return None
 
         return value
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode base64 value"""
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_value = base64.b64decode(base64_authorization_header)
+        except Exception as e:
+            return None
+
+        return decoded_value.decode("utf-8")
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> Tuple[str, str]:
+        """extract username and password"""
+        if decoded_base64_authorization_header is None or not isinstance(
+                decoded_base64_authorization_header, str):
+            return None, None
+
+        email = ""
+        password = ""
+        semicolon = ""
+        for char in decoded_base64_authorization_header:
+            if char == ":":
+                semicolon = char
+            elif semicolon == "":
+                email += char
+            else:
+                password += char
+
+        if semicolon == "":
+            return None, None
+
+        return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """retrieve user instance"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        if User.all():
+            attribute_list = User.search({'email': user_email})
+            if len(attribute_list) == 0:
+                return None
+
+            user_instance = attribute_list[0]
+            if user_instance.is_valid_password(user_pwd) is False:
+                return None
+
+            return user_instance
+
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """complete basic auth operation"""
+        auth_value = self.authorization_header(request)
+        encoded_value = self.extract_base64_authorization_header(auth_value)
+        decoded_value = self.decode_base64_authorization_header(encoded_value)
+        credentials = self.extract_user_credentials(decoded_value)
+        user_instance = self.user_object_from_credentials(credentials)
+
+        return user_instance
